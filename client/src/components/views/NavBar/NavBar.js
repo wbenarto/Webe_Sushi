7f76248d9331be1ff4b7d522d8c973cd596c3405
w.benarto <w.benarto@gmail.com>
2020-06-05 15:18:42 -0700

    logo added

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,6 +3,9 @@ import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
 import './Sections/Navbar.css';
+import { urlencoded } from 'body-parser';
+
+import image from '../../images/WebeCircle.png'
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
@@ -18,7 +21,7 @@ function NavBar() {
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a href="/">Webe Sushi</a>
+        <a href="/"><img className='logo_image' src={image}></img></a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
