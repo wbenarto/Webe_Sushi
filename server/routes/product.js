2b6f5807a211bedff7f74a26c8ed749ecd9a03cc
w.benarto <w.benarto@gmail.com>
2020-09-29 16:29:28 -0700

    router

--- server/routes/product.js
+++ server/routes/product.js
@@ -44,7 +44,7 @@ router.post("/uploadImage", auth, (req, res) => {
     return res.json({
       success: true,
       image: res.req.file.path,
-      filename: res.req.file.filename,
+      fileName: res.req.file.filename,
     });
   });
   // console.log(image)
